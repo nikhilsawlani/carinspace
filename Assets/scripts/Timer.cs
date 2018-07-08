40ec0909752ae9e3bba0dfdf21b95c262a6d3dfd
Nikhil <NIKHIL_SAWLANI@CRGL-THIRDPARTY.COM>
2018-07-08 16:10:32 +0530

    initial commit

---
+++ Assets/scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+	public Text timer;
+	float timeleft = 60.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeleft -= Time.deltaTime;
+		timer.text = timeleft.ToString ("F1");
+		if (timeleft < 0) {
+			SceneManager.LoadScene (1);
+
+		}
+	}
+}
